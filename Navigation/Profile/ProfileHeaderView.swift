14a8756e2b46e9635aeba5e67ebc726c96534361
as-mermaid <121520666+as-mermaid@users.noreply.github.com>
2023-01-21 12:51:51 +0300

    Finished extra task

--- Navigation/Profile/ProfileHeaderView.swift
+++ Navigation/Profile/ProfileHeaderView.swift
@@ -10,7 +10,7 @@ import UIKit
 let imgSize: CGFloat = 150
 let offset1: CGFloat = 16
 let offset2: CGFloat = 27
-let offset3: CGFloat = 34
+//let offset3: CGFloat = 34
 
 class ProfileHeaderView: UIView {
     
@@ -26,10 +26,16 @@ class ProfileHeaderView: UIView {
         return name
     }()
 
-    private lazy var statusText: UITextField = {
-        let status = UITextField()
+    private lazy var statusLabel: UILabel = {
+        let status = UILabel()
         status.translatesAutoresizingMaskIntoConstraints = false
         return status
+    }()
+    
+    private lazy var statusField: UITextField = {
+        let field = UITextField()
+        field.translatesAutoresizingMaskIntoConstraints = false
+        return field
     }()
     
     private lazy var actionButton: UIButton = {
@@ -37,6 +43,8 @@ class ProfileHeaderView: UIView {
         button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
+    
+    private var statusText: String = "Nice place for some quote"
     
     override func setNeedsLayout() {
         super.setNeedsLayout()
@@ -52,24 +60,35 @@ class ProfileHeaderView: UIView {
         profileName.font = UIFont.boldSystemFont(ofSize: 18)
         profileName.textColor = .black
         
-        statusText.text = "Nice place for some quote"
-        statusText.textColor = .gray
-        statusText.font = UIFont(name: "System", size: 14)
+        statusLabel.text = statusText
+        statusLabel.textColor = .gray
+        statusLabel.font = UIFont(name: "System", size: 14)
+        
+        statusField.text = statusText
+        statusField.layer.borderWidth = 1
+        statusField.layer.borderColor = UIColor.black.cgColor
+        statusField.backgroundColor = .white
+        statusField.font = UIFont(name: "System", size: 15)
+        statusField.textColor = .black
+        statusField.layer.cornerRadius = 12
+        statusField.clipsToBounds = true
+        statusField.leftView = UIView (frame: CGRect(x: 0, y: 0, width: 10, height: statusField.frame.height))
+        statusField.leftViewMode = .always
         
         actionButton.backgroundColor = .systemBlue
         actionButton.layer.cornerRadius = 4
-        actionButton.setTitle("Show status", for: .normal)
+        actionButton.setTitle("Set status", for: .normal)
         actionButton.setTitleColor(.white, for: .normal)
         actionButton.layer.shadowOffset = CGSize(width: 4, height: 4)
         actionButton.layer.shadowRadius = 4
         actionButton.layer.shadowColor = UIColor.black.cgColor
         actionButton.layer.shadowOpacity = 0.7
         
-        
         addSubview(image)
         addSubview(profileName)
-        addSubview(statusText)
+        addSubview(statusLabel)
         addSubview(actionButton)
+        addSubview(statusField)
         
         let safeAreaGuide = safeAreaLayoutGuide
         
@@ -83,9 +102,14 @@ class ProfileHeaderView: UIView {
             profileName.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: offset1),
             profileName.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -offset1),
             
-            statusText.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: offset1),
-            statusText.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -offset1),
-            statusText.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -offset3),
+            statusLabel.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: offset1),
+            statusLabel.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -offset1),
+            statusLabel.bottomAnchor.constraint(equalTo: statusField.topAnchor, constant: -offset1),
+            
+            statusField.bottomAnchor.constraint(equalTo: actionButton.topAnchor, constant: -offset1),
+            statusField.leadingAnchor.constraint(equalTo: image.trailingAnchor, constant: offset1),
+            statusField.trailingAnchor.constraint(equalTo: safeAreaGuide.trailingAnchor, constant: -offset1),
+            statusField.heightAnchor.constraint(equalToConstant: 40),
             
             actionButton.topAnchor.constraint(equalTo: image.bottomAnchor, constant: offset1),
             actionButton.leadingAnchor.constraint(equalTo: safeAreaGuide.leadingAnchor, constant: offset1),
@@ -94,10 +118,15 @@ class ProfileHeaderView: UIView {
         ])
         
         actionButton.addTarget(self, action: #selector(buttonPressed(_:)), for: .touchUpInside)
+        statusField.addTarget(self, action: #selector(statusTextChanged(_:)), for: .editingChanged)
         
     }
     
     @objc func buttonPressed(_ sender: UIButton) {
-        print (statusText.text ?? "Empty status")
+        statusLabel.text = statusText
+    }
+    
+    @objc func statusTextChanged(_ sender: UITextField) {
+        statusText = statusField.text ?? ""
     }
 }
